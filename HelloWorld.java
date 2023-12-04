092bc82548f35326d34b8b7eab8c3068cba37e4e
gowthamdommeti123 <72290162+gowthamdommeti123@users.noreply.github.com>
2023-12-04 16:07:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
 
       public static void main(String[] args){
            System.out.println("Hello Jenkins, Good Day!");
+            System.out.println("I am new to DevOps");
            
       }
 }
